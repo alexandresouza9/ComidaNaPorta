9e6314ceda5eae79a34de963eb05cc35c71e8bac
Alexandre Souza <alexandresouza9@gmail.com>
2023-10-02 23:10:25 -0300

    commit
    atualização da tela do cadastro

--- script.js
+++ script.js
@@ -11,19 +11,40 @@ document.addEventListener("DOMContentLoaded", function () {
   const password = document.getElementById("password")
   const passwordconfirmation = document.getElementById("password-confirmation")
 
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault()
-    checkInputUsername();
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    checkForm();
     
     var previousPageURL = document.referrer // Obtém a URL da página anterior
     window.location.href = previousPageURL // Redireciona para a página anterior
   })
+  email.addEventListener("blur", () => {
+    checkInputEmail();
+  })
+
+  username.addEventListener("blur", () => {
+    checkInputUsername();
+  })
+  
 })
 function checkInputUsername(){
   const usernameValue = username.value;
+
   if(usernameValue === ""){
     // mostrar o aviso de erro...
-    
+    errorInput(username, "Preencha um username!")
+  }else{
+    const formItem = username.parentElement;
+    formItem.classList = "form-content"
   }
 }
+
+function errorInput(input, message){
+  const formItem = input.parentElement;
+  const textMessage = formItem.querySelector("a")
+
+  textMessage.innerText = message;
+  formItem.className = "form-content error"
+}
+
